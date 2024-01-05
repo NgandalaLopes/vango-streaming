66d2b02b6aa5e4bfda54d037d54ff49d853bb787
NgandalaLopes <ngandalalopes@gmail.com>
2024-01-04 21:20:01 -0600

    edit toggler

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
@@ -13,27 +12,30 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ThemeToggler() {
-  const { setTheme } = useTheme()
+  const { setTheme, theme } = useTheme()
+
+  const themeOptions = ["light", "dark"]
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun className={`h-[1.2rem] w-[1.2rem] ${theme === 'light' ? 'rotate-0 scale-100' : 'rotate-90 scale-0'} transition-all dark:-rotate-90 dark:scale-0`} />
+          <Moon className={`absolute h-[1.2rem] w-[1.2rem] ${theme === 'dark' ? 'rotate-0 scale-100' : 'rotate-90 scale-0'} transition-all dark:rotate-0 dark:scale-100`} />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
+        {themeOptions.map((option) => {
+          if (option !== theme) {
+            return (
+              <DropdownMenuItem key={option} onClick={() => setTheme(option)}>
+                {option.charAt(0).toUpperCase() + option.slice(1)}
+              </DropdownMenuItem>
+            )
+          }
+          return null
+        })}
       </DropdownMenuContent>
     </DropdownMenu>
   )
